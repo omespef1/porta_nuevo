4cc10ab9b66f2639c85a0877ff2b906199ee4e03
omespef1 <omespef1@hotmail.com>
2019-06-27 17:59:35 -0500

    Initial commit

---
+++ src/app/core/_config/i18n/es.ts
@@ -0,0 +1,105 @@
+// Spain
+export const locale = {
+	lang: 'es',
+	data: {
+		TRANSLATOR: {
+			SELECT: 'Elige tu idioma',
+		},
+		MENU: {
+			NEW: 'nuevo',
+			ACTIONS: 'Comportamiento',
+			CREATE_POST: 'Crear nueva publicación',
+			PAGES: 'Páginas',
+			FEATURES: 'Caracteristicas',
+			APPS: 'Aplicaciones',
+			DASHBOARD: 'Tablero'
+		},
+		AUTH: {
+			GENERAL: {
+				OR: 'O',
+				SUBMIT_BUTTON: 'Enviar',
+				NO_ACCOUNT: 'No tienes una cuenta?',
+				SIGNUP_BUTTON: 'Regístrate',
+				FORGOT_BUTTON: 'Se te olvidó tu contraseña',
+				BACK_BUTTON: 'Atrás',
+				PRIVACY: 'Intimidad',
+				LEGAL: 'Legal',
+				CONTACT: 'Contacto',
+			},
+			LOGIN: {
+				TITLE: 'Crear una cuenta',
+				BUTTON: 'Registrarse',
+			},
+			FORGOT: {
+				TITLE: 'Contraseña olvidada?',
+				DESC: 'Ingrese su correo electrónico para restablecer su contraseña',
+				SUCCESS: 'Tu cuenta ha sido restaurada exitosamente'
+			},
+			REGISTER: {
+				TITLE: 'Regístrate',
+				DESC: 'Ingresa tus datos para crear una cuenta',
+				SUCCESS: 'Tu cuenta ha sido creada exitosamente'
+			},
+			INPUT: {
+				EMAIL: 'Email',
+				FULLNAME: 'Nomber completo',
+				PASSWORD: 'Contraseña',
+				CONFIRM_PASSWORD: 'Confirmar contraseña',
+				USERNAME: 'Usuario'
+			},
+			VALIDATION: {
+				INVALID: '{{name}} no es válido',
+				REQUIRED: '{{name}} es requerido',
+				MIN_LENGTH: '{{name}} Longitud mínima debe ser {{min}}',
+				AGREEMENT_REQUIRED: 'Debes aceptar los términos y condiciones',
+				NOT_FOUND: 'The requested {{name}} is not found',
+				INVALID_LOGIN: 'Los datos para acceder son incorrectos',
+				REQUIRED_FIELD: 'Campo requerido',
+				MIN_LENGTH_FIELD: 'Minimum field length:',
+				MAX_LENGTH_FIELD: 'Maximum field length:',
+				INVALID_FIELD: 'Field is not valid',
+			}
+		},
+		ECOMMERCE: {
+			COMMON: {
+				SELECTED_RECORDS_COUNT: 'Selected records count: ',
+				ALL: 'All',
+				SUSPENDED: 'Suspended',
+				ACTIVE: 'Active',
+				FILTER: 'Filter',
+				BY_STATUS: 'by Status',
+				BY_TYPE: 'by Type',
+				BUSINESS: 'Business',
+				INDIVIDUAL: 'Individual',
+				SEARCH: 'Search',
+				IN_ALL_FIELDS: 'in all fields'
+			},
+			ECOMMERCE: 'eCommerce',
+			CUSTOMERS: {
+				CUSTOMERS: 'Customers',
+				CUSTOMERS_LIST: 'Customers list',
+				NEW_CUSTOMER: 'New Customer',
+				DELETE_CUSTOMER_SIMPLE: {
+					TITLE: 'Customer Delete',
+					DESCRIPTION: 'Are you sure to permanently delete this customer?',
+					WAIT_DESCRIPTION: 'Customer is deleting...',
+					MESSAGE: 'Customer has been deleted'
+				},
+				DELETE_CUSTOMER_MULTY: {
+					TITLE: 'Customers Delete',
+					DESCRIPTION: 'Are you sure to permanently delete selected customers?',
+					WAIT_DESCRIPTION: 'Customers are deleting...',
+					MESSAGE: 'Selected customers have been deleted'
+				},
+				UPDATE_STATUS: {
+					TITLE: 'Status has been updated for selected customers',
+					MESSAGE: 'Selected customers status have successfully been updated'
+				},
+				EDIT: {
+					UPDATE_MESSAGE: 'Customer has been updated',
+					ADD_MESSAGE: 'Customer has been created'
+				}
+			}
+		}
+	}
+};
